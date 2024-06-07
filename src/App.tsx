4afd922d2a6b7692b7775fba6f23e5d3126f03bb
aman <amanagarwalx123@gmail.com>
2024-06-07 19:20:15 +0530

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ const App: React.FC = () => {
   // Initializing state with a default location
   const [lat, setLat] = useState<number>(defaultCoordinates.lat);
   const [lon, setLon] = useState<number>(defaultCoordinates.lon);
-  const [tempCoordinates, setTempCoordinates] = useState<[number, number]>([51.5072, -0.1276]);
+  const [tempCoordinates, setTempCoordinates] = useState<[number, number]>([defaultCoordinates.lat, defaultCoordinates.lon]);
 
   const handleFormSubmit = (event: React.FormEvent) => {
     event.preventDefault();
@@ -43,43 +43,74 @@ const App: React.FC = () => {
   const handleLocationSelect = (lat: number, lon: number) => {
     setLat(() => lat);
     setLon(() => lon);
-
+    defaultCoordinates.lat = lat
+    defaultCoordinates.lon = lon
+    setTempCoordinates([lat, lon])
   };
 
   return (
-    <div style={{
-      backgroundImage: `url(${BgWeather})`,
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-      backgroundRepeat: 'no-repeat'
-    }} className=" h-screen mx-auto " >
-      <header className="text-2xl gap-2 h-16 font-medium mb-4 w-full bg-gradient-to-r from-indigo-800 to-blue-800  text-white flex items-center justify-start pl-8">
-        <img src={Logo} alt="Icon" className='h-10' />
+    <div
+      style={{
+        backgroundImage: `url(${BgWeather})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat'
+      }}
+      className="h-screen mx-auto"
+    >
+      <header className="text-2xl gap-2 h-16 font-medium mb-4 w-full bg-gradient-to-r from-indigo-800 to-blue-800 text-white flex items-center justify-start pl-8">
+        <img src={Logo} alt="Icon" className="h-10" />
         <p className="bg-gradient-to-r from-fuchsia-300 to-amber-300 bg-clip-text text-transparent">
           WeatherX
         </p>
       </header>
-      <form onSubmit={handleFormSubmit} className="mb-4 gap-4 flex items-center justify-center ">
+      <form
+        onSubmit={handleFormSubmit}
+        className="mb-4 gap-4 flex flex-col md:flex-row items-center justify-center px-4 md:px-0"
+      >
         {/* Latitude Input */}
-        <label className="block mb-2 text-lg font-semibold">
+        <label className="block mb-2 md:mb-0 text-lg font-semibold">
           Latitude
-          <input name="latitude" type="number" value={tempCoordinates[0]} onChange={(e) => setTempCoordinates([Number(e.target.value), tempCoordinates[1]])} required className="ml-4 border border-black rounded-md p-2 bg-neutral-100 text-sm font-normal outline-none" />
+          <input
+            name="latitude"
+            type="number"
+            value={tempCoordinates[0]}
+            onChange={(e) =>
+              setTempCoordinates([Number(e.target.value), tempCoordinates[1]])
+            }
+            required
+            className="ml-0 md:ml-4 border border-black rounded-md p-2 bg-neutral-100 text-sm font-normal outline-none w-full md:w-auto"
+          />
         </label>
         {/* Longitude Input */}
-        <label className="block mb-2 text-lg font-semibold">
+        <label className="block mb-2 md:mb-0 text-lg font-semibold">
           Longitude
-          <input name="longitude" type="number" value={tempCoordinates[1]} onChange={(e) => setTempCoordinates([tempCoordinates[0], Number(e.target.value)])} required className="ml-4 border border-black rounded-md p-2 bg-neutral-100 text-sm font-normal outline-none" />
+          <input
+            name="longitude"
+            type="number"
+            value={tempCoordinates[1]}
+            onChange={(e) =>
+              setTempCoordinates([tempCoordinates[0], Number(e.target.value)])
+            }
+            required
+            className="ml-0 md:ml-4 border border-black rounded-md p-2 bg-neutral-100 text-sm font-normal outline-none w-full md:w-auto"
+          />
         </label>
         {/* Location input search */}
-        <div className=" items-center justify-center inline-flex ">
-          <p className='block text-lg mb-2 font-semibold'>Location</p>
-          <div className='ml-4 p-1 text-center text-sm'>
+        <div className="flex flex-col md:flex-row items-center justify-center md:inline-flex">
+          <p className="block text-lg mb-2 md:mb-0 font-semibold">Location</p>
+          <div className="ml-0 md:ml-4 p-1 text-center text-sm w-full md:w-auto">
             <LocationSearchInput onLocationSelect={handleLocationSelect} />
           </div>
         </div>
-        <button type="submit" className="ml-5 mb-2 p-2 px-4 ring-4 ring-emerald-500 ring-offset-2 font-light hover:text-white hover:font-semibold  text-white hover:bg-neutral-700 bg-neutral-800 transition-all rounded-lg">Get Weather</button>
+        <button
+          type="submit"
+          className="mt-2 md:mt-0 ml-0 md:ml-5 mb-2 p-2 px-4 ring-4 ring-emerald-500 ring-offset-2 font-light hover:text-white hover:font-semibold text-white hover:bg-neutral-700 bg-neutral-800 transition-all rounded-lg"
+        >
+          Get Weather
+        </button>
       </form>
-      <div className='flex items-end justify-center gap-4 relative z-0 mt-2 '>
+      <div className="flex flex-col md:flex-row items-end justify-center gap-4 relative z-0 mt-2 px-4 md:px-0">
         {/* Weather display */}
         <WeatherDisplay lat={lat} lon={lon} />
         {/* Map display */}
@@ -89,6 +120,7 @@ const App: React.FC = () => {
       <RecentSearches lat={lat} lon={lon} onLocationSelect={handleLocationSelect} />
     </div>
   );
-};
+}
 
 export default App;
+
